27e99e0c2ee33e2d134c1b645aaf726680c99f7c
Micaela Rombola <rombolamicaela@gmail.com>
2023-12-11 20:59:23 -0300

    bucle

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-let nombre= prompt("Ingrese su nombre");
-let apellido=prompt("Ingrese su apellido");
-let materia=prompt("ingrese su materia");
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let materia = prompt("Ingrese su materia");
 alert(`Bienvenida ${nombre} ${apellido} Su materia es: ${materia}`);
 
-// Función para determinar si una entrega fue aprobada o desaprobada
-function resultadoPreEntrega(nota) {
-    return nota >= 7 ? "Aprobaste la pre-entrega" : "Desaprobaste la pre-entrega";
-}
+let notaPreEntrega;
 
-// Solicitar al usuario ingresar la nota de la pre-entrega
-let notaPreEntrega = parseFloat(prompt("Ingresa la nota de la pre-entrega:"));
+// Utilizar un bucle while para solicitar la nota hasta que se ingrese una entrada válida mayor o igual a 7
+while (true) {
+    // Solicitar al usuario ingresar la nota de la pre-entrega
+    let input = prompt("Ingresa la nota de la pre-entrega:");
+    notaPreEntrega = parseFloat(input);
 
-// Verificar si la entrada es válida
-if (!isNaN(notaPreEntrega)) {
-    // Obtener el resultado de la pre-entrega usando la función
-    let mensajeResultado = resultadoPreEntrega(notaPreEntrega);
+    // Verificar si la entrada es válida
+    if (!isNaN(notaPreEntrega)) {
+        if (notaPreEntrega >= 7) {
+            // Mostrar el mensaje de resultado
+            alert("Aprobado");
+        } else {
+            // Mostrar el mensaje de resultado
+            alert("Desaprobado");
+        }
 
-    // Mostrar el mensaje de resultado
-    alert(mensajeResultado);
-} else {
-    alert("Desaprobaste");
+        break; // Salir del bucle si la entrada es válida
+    } else {
+        alert("Por favor, ingrese una nota válida.");
+    }
 }
